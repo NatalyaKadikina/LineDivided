62b0c76d58ee94bb89c8a489279094ae5eb640e4
Наталья Кадикина <natalya92filipp@gmail.com>
2023-01-27 19:45:49 +0300

    сделано

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,10 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-
+        String splitStr = "Код должен работать для строк любого размера и любого содержания";
+        String[] words = splitStr.split(" ");
+        for (String word : words) {
+            System.out.println(word);
+        }
     }
 }
